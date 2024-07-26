0221df1bae0143a5bb5bbb28f31bcf8ec1cbfad2
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-07-26 16:40:39 +0200

    Solution + Test

--- src/main/java/com/ismaelmohamedbouh/Solution.java
+++ src/main/java/com/ismaelmohamedbouh/Solution.java
@@ -1,4 +1,67 @@
 package com.ismaelmohamedbouh;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 class Solution {
+
+    public boolean canFinish(int nombreDeCours, int[][] prerequis) {
+        List<List<Integer>> graphe = construireGraphe(nombreDeCours, prerequis);
+        int[] degresEntrants = calculerDegresEntrants(nombreDeCours, prerequis);
+
+        Deque<Integer> file = initialiserFile(nombreDeCours, degresEntrants);
+
+        int coursTraites = traiterCours(graphe, degresEntrants, file);
+
+        return coursTraites == nombreDeCours;
+    }
+
+    private List<List<Integer>> construireGraphe(int nombreDeCours, int[][] prerequis) {
+        List<List<Integer>> graphe = new ArrayList<>();
+        for (int i = 0; i < nombreDeCours; i++) {
+            graphe.add(new ArrayList<>());
+        }
+        for (int[] prerequi : prerequis) {
+            int cours = prerequi[0];
+            int coursPrerequi = prerequi[1];
+            graphe.get(coursPrerequi).add(cours);
+        }
+        return graphe;
+    }
+
+    private int[] calculerDegresEntrants(int nombreDeCours, int[][] prerequis) {
+        int[] degresEntrants = new int[nombreDeCours];
+        for (int[] prerequi : prerequis) {
+            int cours = prerequi[0];
+            degresEntrants[cours]++;
+        }
+        return degresEntrants;
+    }
+
+    private Deque<Integer> initialiserFile(int nombreDeCours, int[] degresEntrants) {
+        Deque<Integer> file = new ArrayDeque<>();
+        for (int i = 0; i < nombreDeCours; i++) {
+            if (degresEntrants[i] == 0) {
+                file.offer(i);
+            }
+        }
+        return file;
+    }
+
+    private int traiterCours(List<List<Integer>> graphe, int[] degresEntrants, Deque<Integer> file) {
+        int coursTraites = 0;
+        while (!file.isEmpty()) {
+            int cours = file.poll();
+            coursTraites++;
+            for (int voisin : graphe.get(cours)) {
+                degresEntrants[voisin]--;
+                if (degresEntrants[voisin] == 0) {
+                    file.offer(voisin);
+                }
+            }
+        }
+        return coursTraites;
+    }
 }
--- src/test/java/com/ismaelmohamedbouh/SolutionTest.java
+++ src/test/java/com/ismaelmohamedbouh/SolutionTest.java
@@ -1,5 +1,35 @@
 package com.ismaelmohamedbouh;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SolutionTest {
 
+    private final Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("fournirCasDeTest")
+    public void testCanFinish(int nombreDeCours, int[][] prerequis, boolean attendu) {
+        assertEquals(attendu, solution.canFinish(nombreDeCours, prerequis));
+    }
+
+    private static Stream<Arguments> fournirCasDeTest() {
+        return Stream.of(
+            Arguments.of(2, new int[][]{{1, 0}}, true), // Cas simple, un cours dépend d'un autre
+            Arguments.of(2, new int[][]{{1, 0}, {0, 1}}, false), // Cycle, impossible de terminer les cours
+            Arguments.of(4, new int[][]{{1, 0}, {2, 1}, {3, 2}}, true), // Chaîne linéaire de dépendances
+            Arguments.of(3, new int[][]{{1, 0}, {2, 1}, {2, 0}}, true), // Plusieurs prérequis pour un cours
+            Arguments.of(3, new int[][]{{1, 0}, {2, 1}, {1, 2}}, false), // Cycle indirect
+            Arguments.of(5, new int[][]{{0, 1}, {0, 2}, {0, 3}, {0, 4}}, true), // Un cours requis par plusieurs autres
+            Arguments.of(1, new int[][]{}, true), // Un seul cours, aucun prérequis
+            Arguments.of(5, new int[][]{}, true), // Plusieurs cours, aucun prérequis
+            Arguments.of(3, new int[][]{{0, 1}, {0, 2}, {1, 2}}, true), // Pré-requis convergents vers un seul cours
+            Arguments.of(6, new int[][]{{1, 0}, {2, 1}, {3, 2}, {4, 3}, {5, 4}, {3, 5}}, false) // Cycle complexe
+        );
+    }
 }
